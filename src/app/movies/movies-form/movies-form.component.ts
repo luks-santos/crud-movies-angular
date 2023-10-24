5ad87cabb93a16b55953656402b7782994d66ccc
Lucas Batista <lucas.bt46@gmail.com>
2023-10-24 17:34:53 -0300

    criação formulario cadastro de filmes html css

--- src/app/movies/movies-form/movies-form.component.ts
+++ src/app/movies/movies-form/movies-form.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-movies-form',
   templateUrl: './movies-form.component.html',
   styleUrls: ['./movies-form.component.scss']
 })
-export class MoviesFormComponent {
+export class MoviesFormComponent implements OnInit {
 
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('salvar');
+    
+  }
+
+  onCancel() {
+    console.log('cancelar');
+    
+  }
 }
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { MoviesFormComponent } from './movies-form/movies-form.component';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies/movies.component';
-import { MoviesFormComponent } from './movies-form/movies-form.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MoviesFormComponent } from './movies-form/movies-form.component';
     CommonModule,
     MoviesRoutingModule,
     AppMaterialModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class MoviesModule { }
